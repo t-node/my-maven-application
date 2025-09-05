b9bab3b0a4107731a378497f71d4adfb2f728a9b
vevin.moza <vevin.moza@gmail.com>
2025-09-05 14:40:04 +0530

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+    }
+
+    public static String getMessage() {
+        return "Hello, GitHub Actions!";
+    }
+}
