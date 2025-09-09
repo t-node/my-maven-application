e9188baa25464ad0b0fdd822c12f6c0521f5f3cd
vevin.moza <vevin.moza@gmail.com>
2025-09-09 13:44:17 +0530

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        String who = System.getenv().getOrDefault("WHO", "GitHub Actions are new");
+        String who = System.getenv().getOrDefault("WHO", "GitHub Actions are new for the first time");
         System.out.println(hello(who));
     }
 }
