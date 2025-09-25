6c32a1cc48944cbe8e0d8cbd74649286d15a1342
vevin.moza <vevin.moza@gmail.com>
2025-09-26 00:12:34 +0530

    added space

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        String who = System.getenv().getOrDefault("WHOEAASSddRRE", "GitHub Actions are new for the first time oddnly from pr here");
+        String who = System.getenv().getOrDefault("WHOEAASssSddRRE", "GitHub Actions are new for the first time oddnly from pr here");
         System.out.println(hello(who));
     }
 }
